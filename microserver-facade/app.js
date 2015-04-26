424ef90786ea3a77ab677159a6154dcd6c93676f
Jason Plumb <jason@noisybox.net>
2015-04-26 14:39:51 -0700

    Clarify startup text

--- microserver-facade/app.js
+++ microserver-facade/app.js
@@ -17,5 +17,5 @@ app.get('/', function (req, res) {
 var server = app.listen(PORT, function () {
 	var host = server.address().address;
 	var port = server.address().port;
-	console.log('Example app listening at http://%s:%s', host, port);
+	console.log('Microserver facade listening at http://%s:%s', host, port);
 });
