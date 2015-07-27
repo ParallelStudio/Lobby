25951aa8ac521c03349e1bf2d68526799333d5a4
Jason Plumb <jason@noisybox.net>
2015-07-26 22:03:09 -0700

    Send OSC

--- volume-server/app.js
+++ volume-server/app.js
@@ -1,9 +1,19 @@
 var fs = require('fs');
+var osc = require('node-osc');
 var express = require('express');
 var app = express();
 
 var SAVESTATE = 'state.json';
 var CREDS = 'creds.json';
+
+if(process.argv.length <= 1){
+	return usage();
+}
+
+var args = process.argv.slice(2);
+var hostport = args[0].split(/:/);
+console.log("Opening OSC client to " + hostport[0] + " on port " + hostport[1]);
+var client = new osc.Client(hostport[0], hostport[1]);
 
 var state = loadPersistedValues();
 var creds = JSON.parse(fs.readFileSync(CREDS));
@@ -28,6 +38,9 @@ app.get('/', auth, function(req, res){
 
 app.post('/vol/:zone', auth, function(req, res){
 	console.log("Changing zone: " + req.params.zone + " to " + req.body);
+	client.send('/' + req.params.zone, parseInt(req.body), function(){
+		//nop
+	});
 	state[req.params.zone] = req.body;
 	saveState(state);
 	res.set('Conent-Type', 'text/plain');
@@ -46,4 +59,9 @@ function loadPersistedValues(){
 
 function saveState(state){
 	fs.writeFileSync(SAVESTATE, JSON.stringify(state));
+}
+
+function usage(){
+	console.log("Usage: node app.js <host>:<port>");
+	console.log("  where <host>:<port> is the OSC target.");
 }
